3d5522c317c15bd55b1e7dfb08eb78edf9819f88
Gabriel <gabriellossoficial@gmail.com>
2025-11-06 15:02:14 -0300

    versão q n usa a libc

---
+++ Aula_3/comp_linux_.s
@@ -0,0 +1,33 @@
+#verificar qual o maior de dois valores
+.section .data
+num1: .word 10 
+num2: .word 5
+
+.section .text
+    .globl _start
+_start:
+    # Inicializar o gp
+    .option push #empilha as configurações originais
+    .option norelax #desabilita a otimização
+    la gp, _global_pointer$ # script de linkagem padrao, sem otimização para fazer certo
+    .option pop #volta para as configurações normais
+
+
+    #lê o endereço das variaveis globais
+    la t1, num1
+    la t2, num2
+
+    #le o conteudo das variaveis
+    lw t3, 0(t1)
+    lw t4, 0(t2)
+
+    # testa qual das duas é maior
+    bgt t3, t4, t3_maior # if t3 > t4 then t3_maior
+    addi t0, t4, 0 #mv t0, t4 é equivalente?
+
+t3_maior:
+    addi t0, t3, 0
+
+fim:
+    addi t0, t0, 0
+    j fim
